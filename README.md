b7daac56504d71766b7756020c3b3b7e715ba4ad
Osayimwense Izinyon <42164909+GodsentIzzy123@users.noreply.github.com>
2025-05-06 02:07:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# How to run?
+### Step 1  Create a conda environment after cloning the repo
+
+```bash
+conda create -n llamapp python=3.10 -y
+```
+
+```bash
+conda activate llamapp
+```
+
+
+### STEP 02 - Install the requirements
+
+```bash
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "Generative AI Chatbot Project",
+    version = "0.0.1",
+    author = "Osayimwense Izinyon",
+    author_email = "gzz4346@wmich.edu",
+    packages = find_packages(),
+    install_requires =[]
+
+
+
+)
